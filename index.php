244d5ed9bd62adf602a90fa1c1b2dc8725c33fbe
riniroyanti08 <rroyantimarbun@gmail.com>
2021-04-20 18:03:53 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form>
+        <h2>Login</h2>
+        <label>User Name</label>
+        <label>Password</label>
+    <form>
+</body>
+</html>
