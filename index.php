63f0411b9f40941a4628696d2594f9900f716660
riniroyanti08 <rroyantimarbun@gmail.com>
2021-04-20 18:06:18 +0700

    Menambahkan <form> file index.php

--- index.php
+++ index.php
@@ -7,8 +7,17 @@
 <body>
     <form>
         <h2>Login</h2>
+         <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+        <?php } ?>
+
         <label>User Name</label>
+        <input type="text" name="uname" placeholder="User Name">
+
         <label>Password</label>
+        <input type="password" name="password" placeholder="Password">
+
+        <button type="submit">Login</button>
     <form>
 </body>
 </html>
